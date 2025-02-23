401f85631bd53c60fa2c3855acb88c2b6e2d371e
NavX <navjotkaur1440@gmail.com>
2025-02-23 15:42:58 +0530

    Add files via upload

---
+++ createmodule/calculator.js
@@ -0,0 +1,12 @@
+export let sumData = (a,b) =>{            // made a calculator module and function sumData in it
+    return a + b;
+};
+
+export let minData = (a,b) =>{             // to write export in the front is also called named export
+    return a - b;
+};
+
+// export default sumData;                // exporting function by default method
+// export {sumData, minData};                           // exporting function by named exporting method
+
+export let cName = "Navjot Kaur";
---
+++ createmodule/index.js
@@ -0,0 +1,5 @@
+import {sumData as sum, minData, cName} from "./calculator.js";               //use curly braces for named exports
+
+console.log(sum(10,20));                                                 // using aliasing
+console.log(minData(40,20));
+console.log(cName);
