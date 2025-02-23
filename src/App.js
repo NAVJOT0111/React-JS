87bed50026bc0cc8c7c1e41734c0e34d5842f453
NavX <navjotkaur1440@gmail.com>
2025-02-23 15:45:47 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  // let name = "Navjot";
+  // let l = [10,20,30,40];
+  // let obj = {
+  //   'name': "WS",
+  //   'cName': "MERN",
+  //   'desc' : "React JS"
+  // let status = true;
+
+  return (
+    <div className="App">
+
+      {/* <h1> {name} </h1>
+      { l.map((v) => {
+        return(
+          <div>{v} </div>
+        )
+      })}
+
+      <div>
+        {obj.cName}
+      </div>
+
+      <div> {10+20} </div>
+      { (status) ?                              //using ternary operator
+      <h1 style={{color:"red", backgroundColor:"yellow"}} >Welcome to WS</h1>       
+       :
+      " "                //true part before the colon and false part after it
+      } */}
+
+    </div>
+  );
+  }
+
+  
+
+
+
+export default App;
