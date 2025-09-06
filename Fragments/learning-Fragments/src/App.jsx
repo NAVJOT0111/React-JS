bf2bf9a7d9206d06624b15d8ec862c00c994c8f1
Navjot Kaur <navjotkaur1440@gmail.com>
2025-09-07 01:36:57 +0530

    next gen commit

--- Fragments/learning-Fragments/src/App.jsx
+++ Fragments/learning-Fragments/src/App.jsx
@@ -1,30 +1,31 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import './App.css';
+import FoodItems from "./components/Fooditems";
+import ErrorMessage from "./components/ErrorMessage";
 
 function App() {
 
-  let foodItems = ["Fruits",'Milk','Green Vegetable','Sabji','Roti'];
+  // let foodItems = ["Fruits",'Milk','Green Vegetable','Sabji','Roti']
   // let foodItems = [];
 
   // if(foodItems.length === 0){
   //   return <h3>I am still Hungry</h3>
   // }
 
-  let emptyMessage = foodItems.length === 0 ? <h3>I am still Hungry</h3> : null ;
+  // let emptyMessage = foodItems.length === 0 ? <h3>I am still Hungry</h3> : null ;
+  // {foodItems.length === 0 ? <h3>I am still Hungry</h3> : null};
+  // {foodItems.length === 0 && <h3>I am still Hungry</h3> }; 
+
 
   return (
     <React.Fragment>
 
       <h1>Healthy Foods</h1>
-
-      {/* {foodItems.length === 0 ? <h3>I am still Hungry</h3> : null} */}
-      {/* {foodItems.length === 0 && <h3>I am still Hungry</h3> }; */}
-      {emptyMessage};
-
-      <ul className="list-group">
-        {foodItems.map(item => <li key={item} class="list-group-item">{item}</li>)}
-      </ul>
+      
+      {/* {emptyMessage} */}
+    <FoodItems> </FoodItems>
+    <ErrorMessage> </ErrorMessage>
 
     </React.Fragment>
   );
